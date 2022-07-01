66f289ace8033573c768b2764782378429654c7d
Williams Momoh <williamsmomoh@gmail.com>
2022-07-01 04:44:58 -0700

    check if num is zero, positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,12 +13,17 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() -RAND_MAX / 2;
-	if (n > 0) {
+	n = rand() - RAND_MAX / 2;
+	if (n > 0) 
+	{
 		printf("is positive\n");
-	} else if (n < 0) {
+	}
+	else if (n < 0) 
+	{
 		printf("is negative\n");
-	} else {
+	}
+       	else
+	{
 		printf("is zero\n");
 	}
 
