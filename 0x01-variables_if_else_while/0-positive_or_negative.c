d79c3a1ecb0a16e050982d5f3611a147a5e35bb1
Williams Momoh <williamsmomoh@gmail.com>
2022-07-01 05:05:37 -0700

    check if num is zero, positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success) 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -21,7 +21,7 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
-       		else
+	else
 	{
 		printf("%d is zero\n", n);
 	}
