32566ead947913804b321ed0fa9ab07274b90abd
Williams Momoh <williamsmomoh@gmail.com>
2022-07-05 12:41:46 -0700

    Print the sign of an integer

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign in a number
+ * @n: is a a single character integer
+ * Return: 1 if is positive, 0 if it's zero, -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
