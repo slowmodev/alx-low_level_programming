c373a47a130d43e610b9c265405d12425b5f9816
Williams Momoh <williamsmomoh@gmail.com>
2022-07-05 11:54:54 -0700

    Update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
+ * print_alphabet_10 - prints a-z in 10 rows
  *
- *
- *
+ * Return: Always 0
  */
 void print_alphabet_x10(void)
 {
 	int i, j;
 	i = 0;
+
 	while (i < 10)
 	{
 		for (j = 'a'; j <= 'z'; j++)
