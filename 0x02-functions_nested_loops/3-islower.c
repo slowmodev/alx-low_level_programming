1db1a9bcc8ab41c100fe00fea9ad5dae612b3299
Williams Momoh <williamsmomoh@gmail.com>
2022-07-05 12:09:13 -0700

    user defined is lower function

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * _islower - a function that checks if a char is a lowercase alphabet
+ * @c: single character input
+ * Return: 1 if int c is lowercase, 0 if other
  */
 int _islower(int c);
 {
