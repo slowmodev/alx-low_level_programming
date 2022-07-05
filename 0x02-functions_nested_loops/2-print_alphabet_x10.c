c3821e267c1ff90504485bbf2473d35ddace966d
Williams Momoh <williamsmomoh@gmail.com>
2022-07-05 11:47:27 -0700

    Print a-z in 10 rows

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+	i = 0;
+	while (i < 10)
+	{
+		for (j => 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		i++
+		_putchar('\n')
+	}
+}
