658a4d22318428da555c79d43be70ac562663fb9
Williams Momoh <williamsmomoh@gmail.com>
2022-07-07 11:39:46 -0700

    Print '\' diagonally

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints \ diagonally
+ * @n: number of times '\' will be printed
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	int i, sp;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (sp = 1; sp < n; sp++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
