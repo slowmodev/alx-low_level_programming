450e7e18e04317856994fb15ee12239b4e39d225
Williams Momoh <williamsmomoh@gmail.com>
2022-07-07 07:31:02 -0700

    Tried to update code

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
- * more_numbers - print 0-14 in 10 lines
+ * more_numbers - checks code
  */
 void more_numbers(void)
 {
 	int i = 0, j;
-	
-	while (i < 10)
+
+	while (i <= 9)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j <= 14;)
 		{
 			if (j >= 10)
 			{
 				_putchar((j / 10) + '0');
 			}
 			_putchar((j % 10) + '0');
+			j++;
 		}
 		i++;
 		_putchar('\n');
