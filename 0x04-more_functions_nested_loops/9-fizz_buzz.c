fb706982719c30c78187301c6c16420020409bad
Williams Momoh <williamsmomoh@gmail.com>
2022-07-07 12:27:55 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * Fizz-Buzz - A test to check for the multiples of 3 and 5
+ * Return: 0
+ */
+
+int _fizzbuzz(int n)
+{
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3 = 0) && (n % 5 = 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 3 = 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 = 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+	}
+	printf('\n');
+	return (0);
+}
