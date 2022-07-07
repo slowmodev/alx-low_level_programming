168dbb8e18d9d18d23d4b8b6696f6d092ce5e6d0
Williams Momoh <williamsmomoh@gmail.com>
2022-07-07 05:17:12 -0700

    more functions and loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints 0 - 9 followed by a newline
+ * Return: 0 always (Success)
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		_putchar(i);
+		_putchar('\n');
+		i++;
+	}
+}
