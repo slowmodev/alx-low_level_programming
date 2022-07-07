e70c1197d502122b816a530aeb2c7d69e068fe86
Williams Momoh <williamsmomoh@gmail.com>
2022-07-07 12:37:48 -0700

    Fizzbuzzing

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,28 +5,29 @@
  * Return: 0
  */
 
-int _fizzbuzz(int n)
+int main(void)
 {
+	int n;
 
 	for (n = 1; n <= 100; n++)
 	{
-		if ((n % 3 = 0) && (n % 5 = 0))
+		if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
-		else if (n % 3 = 0)
+		else if (n % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
-		else if (n % 5 = 0)
+		else if (n % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else
 		{
-			printf("%d", n);
+			printf("%d ", n);
 		}
 	}
-	printf('\n');
+	printf("\n");
 	return (0);
 }
