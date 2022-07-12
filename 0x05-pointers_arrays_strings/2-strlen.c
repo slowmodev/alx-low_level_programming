d032168c404e80488b78ca7d70364d17436acc4c
Williams Momoh <williamsmomoh@gmail.com>
2022-07-12 08:40:03 -0700

    Length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strlen - check the length of a string
+ * @s: string
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		if (i > 9)
+		{
+			_putchar((i % 10) + '0');
+		}
+		else 
+		{
+			_putchar(i);
+		}
+	}
+	return (0);
+}
