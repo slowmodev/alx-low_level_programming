a6250504ff0c7de0650a98841cd7619e9e7ae60f
Williams Momoh <williamsmomoh@gmail.com>
2022-07-12 15:42:46 -0700

    number 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,16 +7,18 @@
 
 void rev_string(char *s)
 {
+	char rev = s[0];
 	int fcounter = 0;
-	int i, n;
+	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
+	while (s[fcounter] != '\0')
 		fcounter++;
-	}
 
-	for (n = (fcounter - 1); n >= 0; n--)
+	for (i = 0; i < fcounter; i++)
 	{
-		_putchar(s[n]);
+		fcounter--;
+		rev = s[i];
+		s[i] = s[fcounter];
+		s[fcounter] = rev;
 	}
 }
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character in a string
+ * @str: string
+ */
+
+void puts2(char *str)
+{
+
