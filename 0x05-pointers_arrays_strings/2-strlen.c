f08872c84a81c423cf24024c5fe7d5dbd9cdccb8
Williams Momoh <williamsmomoh@gmail.com>
2022-07-12 08:49:55 -0700

    String length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -11,15 +11,5 @@ int _strlen(char *s)
 	int i;
 
 	for (i = 0; s[i] != '\0'; ++i)
-	{
-		if (i > 9)
-		{
-			_putchar((i % 10) + '0');
-		}
-		else
-		{
-			_putchar(i);
-		}
-	}
-	return (0);
+	return (i);
 }
