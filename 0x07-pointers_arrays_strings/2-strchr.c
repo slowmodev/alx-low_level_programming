983efe8522ffe4f8b68421509f0cf485931a6188
Williams Momoh <williamsmomoh@gmail.com>
2022-07-18 08:18:17 -0700

    Trying to fix 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr - a function that locates a character in a string
@@ -20,5 +21,5 @@ char *_strchr(char *s, char c)
 			return (p);
 		}
 	}
-	return (0);
+	return (NULL);
 }
