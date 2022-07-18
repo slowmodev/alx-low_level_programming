5fdb7cb340f978dbced59a4885181c88a88ac433
Williams Momoh <williamsmomoh@gmail.com>
2022-07-18 06:02:29 -0700

    pushing task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *_memcpy - function that emulates memcpy()
+ * _memcpy - function that emulates memcpy()
  * @dest: destination pointer
  * @src: source pointer
  * @n: number of bytes
@@ -11,6 +11,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
+	if (dest == NULL)
+	{
+		return NULL;
+	}
 	for (i = 0; i < n; i++)
 	{
 		src[i] = dest[i];
