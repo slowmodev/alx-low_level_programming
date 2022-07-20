600aac0b5a7548a3286faeadb482593dd7b8a22f
Williams Momoh <williamsmomoh@gmail.com>
2022-07-20 10:09:55 -0700

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Checks the code
+ * @x: base
+ * @y: power
+ * Return: returns the value sum
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (_pow_recursion(x, y - 1) * x);
+}
