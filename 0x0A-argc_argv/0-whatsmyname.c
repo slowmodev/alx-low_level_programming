c52c561989f84ed46fbacfc4a8885e0c1fcb2762
Williams Momoh <williamsmomoh@gmail.com>
2022-07-22 06:44:58 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _myname - checks the function
+ * @argc: command line aregument counter
+ * #argv: command line strings
+ */
+int main(int argc, char *argv[])
+{
+	_putchar(*argv[0]);
+	_putchar('\n');
+	return (0);
+}
