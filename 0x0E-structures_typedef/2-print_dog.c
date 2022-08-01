05d8c46b5c9224c5b87507be26a63fb52eaddefa
Williams Momoh <williamsmomoh@gmail.com>
2022-08-01 06:59:35 -0700

    print the value of variables for dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - print the values of the variables for dog
+ * @d: pointer to an item
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (!d)
+		return;
+	printf("Name: ");
+	if (d->name)
+		printf("%s\n", d->name);
+	else
+		printf("(nil)\n");
+	printf("Age: ")
+	if (d->age)
+		printf("%f\n", d->age);
+	else
+		printf("(nil)\n");
+	printf("Owner: ")
+	if (d->owner)
+		printf("%s\n", d->owner);
+	else
+		printf("(nil)\n");
+}
