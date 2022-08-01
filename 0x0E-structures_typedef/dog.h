6d1755051ae784ada6c5dce494f02947dca377b8
Williams Momoh <williamsmomoh@gmail.com>
2022-08-01 04:39:20 -0700

    Struct for dogs

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * struct dog - A struct type on dogs
+ * @name: the dog's name
+ * @age: dog's age
+ * @owner: Dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
