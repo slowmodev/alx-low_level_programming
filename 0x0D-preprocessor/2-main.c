1deea5270928d3284fbc6d00558e53782704d6dc
Williams Momoh <williamsmomoh@gmail.com>
2022-08-01 16:32:33 -0700

    function to print file name followed be a new line use a macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints filename followed by a new line
+ * Return: always (0)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
