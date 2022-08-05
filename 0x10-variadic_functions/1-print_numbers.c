0c4d418ac226e59e48599c1e40cbf9c179061db9
Williams Momoh <williamsmomoh@gmail.com>
2022-08-04 17:04:22 -0700

    FX to add numbers followed by a new line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,8 +16,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(p, int));
-		if (seperator && i != (n - 1))
-			printf("%s", seperator);
+		if (separator && i != (n - 1))
+			printf("%s", separator);
 	}
 	va_end(p);
 	printf("\n");
